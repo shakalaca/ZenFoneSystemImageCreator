0e91d2191661cedaf5dc616649a6f171ba58f7ff
Shaka Huang <shakalaca@gmail.com>
2014-11-04 14:32:52 +0800

    Support fonpad note 6
    Signed-off-by: Shaka Huang <shakalaca@gmail.com>

--- scripts/setup.bash
+++ scripts/setup.bash
@@ -6,9 +6,9 @@
 
 # ZenFone 5
 LINK_PERM_SETUP_FILE=link_and_set_perm_zenfone_5
-ROM_URL=http://dlcdnet.asus.com/pub/ASUS/ZenFone/A500CG/UL_ASUS_T00F_WW_1_17_40_16.zip
-#ROM_URL=http://dlcdnet.asus.com/pub/ASUS/ZenFone/A500CG/UL_ASUS_T00F_TW_1_17_40_16.zip
-ZIP_FILE=UL-ASUS_T00F-WW-1.17.40.16-user.zip
+#ROM_URL=http://dlcdnet.asus.com/pub/ASUS/ZenFone/A500CG/UL_ASUS_T00F_WW_1_17_40_16.zip
+ROM_URL=http://dlcdnet.asus.com/pub/ASUS/ZenFone/A500CG/UL_ASUS_T00F_TW_1_17_40_16.zip
+ZIP_FILE=UL-ASUS_T00F-TW-1.17.40.16-user.zip
 SYSTEM_SIZE=1363148800
 
 # ZenFone 6
@@ -18,3 +18,10 @@ SYSTEM_SIZE=1363148800
 #ZIP_FILE=UL-ASUS_T00G-TW-1.17.40.16-user.zip
 #SYSTEM_SIZE=1363148800
 
+# Fonepad Note 6
+#LINK_PERM_SETUP_FILE=link_and_set_perm_fonepad_note_6
+#ROM_URL=http://dlcdnet.asus.com/pub/ASUS/EeePAD/ME560CG/UpdateLauncher_K00G_TW_11_2_1_22.zip
+#ROM_URL=http://dlcdnet.asus.com/pub/ASUS/EeePAD/ME560CG/UpdateLauncher_K00G_WW_11_2_1_22.zip
+#ZIP_FILE=UL-K00G-TW-11.2.1.22-user.zip
+#SYSTEM_SIZE=2147483648
+#FILE_CONTEXT=file_contexts_fonepad_note_6
