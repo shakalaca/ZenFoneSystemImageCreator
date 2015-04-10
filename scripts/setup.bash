81112ec96ea7f278ed6c5a8ebd0d155e461dfdb2
Shaka Huang <shakalaca@gmail.com>
2015-04-10 16:16:59 +0800

    Update Z00A 2.13.40.13 link
    Signed-off-by: Shaka Huang <shakalaca@gmail.com>

--- scripts/setup.bash
+++ scripts/setup.bash
@@ -6,6 +6,6 @@
 
 # ZenFone 2 Z00A
 LINK_PERM_SETUP_FILE=link_and_set_perm_z00a
-ROM_URL=http://dlcdnet.asus.com/pub/ASUS/ZenFone/ZE551ML/UL-Z00A-WW-2.12.40.11-user.zip
+ROM_URL=http://dlcdnet.asus.com/pub/ASUS/ZenFone/UL-Z00A-WW-2.13.40.13-user.zip
 SYSTEM_SIZE=2415919104
 FILE_CONTEXT=file_contexts_z00a
