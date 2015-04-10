1c991cab98ef17a0ce8bef9698feb1488c33d813
Shaka Huang <shakalaca@gmail.com>
2015-04-10 18:29:05 +0800

    Add ZenFone2 Z008
    Signed-off-by: Shaka Huang <shakalaca@gmail.com>

--- scripts/setup.bash
+++ scripts/setup.bash
@@ -9,3 +9,9 @@ LINK_PERM_SETUP_FILE=link_and_set_perm_z00a
 ROM_URL=http://dlcdnet.asus.com/pub/ASUS/ZenFone/UL-Z00A-WW-2.13.40.13-user.zip
 SYSTEM_SIZE=2415919104
 FILE_CONTEXT=file_contexts_z00a
+
+# ZenFone 2 Z008
+#LINK_PERM_SETUP_FILE=link_and_set_perm_z008
+#ROM_URL=http://dlcdnet.asus.com/pub/ASUS/ZenFone/ZE550ML/UL-Z008-WW-2.13.40.11-user.zip
+#SYSTEM_SIZE=2415919104
+#FILE_CONTEXT=file_contexts_z008
