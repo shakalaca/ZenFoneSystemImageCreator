a262d806c491d46ff5cd2567ed38bcf11634e1cb
Shaka Huang <shaka@cs03.org>
2015-04-28 17:06:55 +0800

    Fix typo
    Correct URL of Z00A

--- scripts/setup.bash
+++ scripts/setup.bash
@@ -7,7 +7,7 @@
 # ZenFone 2 Z00A
 LINK_PERM_SETUP_FILE=link_and_set_perm_z00a
 # ROM_URL=http://dlcdnet.asus.com/pub/ASUS/ZenFone/UL-Z00A-WW-2.13.40.13-user.zip
-ROM_URL=http://dlcdnet.asus.com/pub/ASUS/ZenFone/UL-Z00A-WW-2.14.40.19-user.zip
+ROM_URL=http://dlcdnet.asus.com/pub/ASUS/ZenFone/ZE551ML/UL-Z00A-WW-2.14.40.19-user.zip
 SYSTEM_SIZE=2415919104
 FILE_CONTEXT=file_contexts_z00a
 # OTA_URL=http://fota.asus.com/delta_package/phone/WW_Z00A/formal/WW_2.13.40.13_20150401_8390_user_rel-user-WW_Z00A-WW_2.14.40.19_20150415_9660_user_rel-user-WW_Z00A-20150415/WW_551_0401_0415_20150415.zip
