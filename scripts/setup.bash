c93cf5f61840e472a0bd4c4b39459e09f27bdd0d
Shaka Huang <shakalaca@gmail.com>
2015-05-11 22:22:29 +0800

    Update to 2.15.40.13
    Signed-off-by: Shaka Huang <shakalaca@gmail.com>

--- scripts/setup.bash
+++ scripts/setup.bash
@@ -6,16 +6,14 @@
 
 # ZenFone 2 Z00A
 LINK_PERM_SETUP_FILE=link_and_set_perm_z00a
-# ROM_URL=http://dlcdnet.asus.com/pub/ASUS/ZenFone/UL-Z00A-WW-2.13.40.13-user.zip
-ROM_URL=http://dlcdnet.asus.com/pub/ASUS/ZenFone/ZE551ML/UL-Z00A-WW-2.14.40.19-user.zip
+ROM_URL=http://dlcdnet.asus.com/pub/ASUS/ZenFone/ZE551ML/UL-Z00A-WW-2.15.40.10-user.zip
 SYSTEM_SIZE=2415919104
 FILE_CONTEXT=file_contexts_z00a
-# OTA_URL=http://fota.asus.com/delta_package/phone/WW_Z00A/formal/WW_2.13.40.13_20150401_8390_user_rel-user-WW_Z00A-WW_2.14.40.19_20150415_9660_user_rel-user-WW_Z00A-20150415/WW_551_0401_0415_20150415.zip
+OTA_URL=http://fota.asus.com/delta_package/phone/WW_Z00A/formal/WW_2.15.40.10_20150425_1396_user_rel-user-WW_Z00A-WW_2.15.40.13_20150506_3876_user_rel-user-WW_Z00A-20150507/WW_551_0425_0506_20150507.zip
 
 # ZenFone 2 Z008
 #LINK_PERM_SETUP_FILE=link_and_set_perm_z008
-#ROM_URL=http://dlcdnet.asus.com/pub/ASUS/ZenFone/ZE550ML/UL-Z008-WW-2.13.40.11-user.zip
-#ROM_URL=http://dlcdnet.asus.com/pub/ASUS/ZenFone/ZE550ML/UL-Z008-WW-2.14.40.17-user.zip
+#ROM_URL=http://dlcdnet.asus.com/pub/ASUS/ZenFone/ZE550ML/UL-Z008-WW-2.15.40.11-user.zip
 #SYSTEM_SIZE=2415919104
 #FILE_CONTEXT=file_contexts_z008
-#OTA_URL=http://fota.asus.com/delta_package/phone/WW_Z008/formal/WW_2.13.40.11_20150401_0995_user_rel-user-WW_Z008-WW_2.14.40.17_20150414_5574_user_rel-user-WW_Z008-20150415/WW_550_0401_0414_20150415.zip
+#OTA_URL=http://fota.asus.com/delta_package/phone/WW_Z008/formal/WW_2.15.40.11_20150425_4731_user_rel-user-WW_Z008-WW_2.15.40.13_20150507_4648_user_rel-user-WW_Z008-20150507/WW_550_0425_0507_20150507.zip
