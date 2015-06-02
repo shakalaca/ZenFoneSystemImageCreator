4d2e79f03280647742f3564f196abd7612b64284
Shaka Huang <shakalaca@gmail.com>
2015-06-02 16:09:37 +0800

    Update to 2.22.40.x
    Signed-off-by: Shaka Huang <shakalaca@gmail.com>

--- scripts/setup.bash
+++ scripts/setup.bash
@@ -6,12 +6,12 @@
 
 # ZenFone 5
 LINK_PERM_SETUP_FILE=link_and_set_perm_zenfone_5
-ROM_URL=http://dlcdnet.asus.com/pub/ASUS/ZenFone/A500CG/UL-ASUS_T00F-TW-2.20.40.9-user.zip
+ROM_URL=http://dlcdnet.asus.com/pub/ASUS/ZenFone/A500CG/UL-ASUS_T00F-TW-2.22.40.53-user.zip
 SYSTEM_SIZE=1363148800
 FILE_CONTEXT=file_contexts_zenfone
 
 # ZenFone 6
 #LINK_PERM_SETUP_FILE=link_and_set_perm_zenfone_6
-#ROM_URL=http://dlcdnet.asus.com/pub/ASUS/ZenFone/A600CG/UL-ASUS_T00G-TW-2.20.40.8-user.zip
+#ROM_URL=http://dlcdnet.asus.com/pub/ASUS/ZenFone/A600CG/UL-ASUS_T00G-TW-2.22.40.44-user.zip
 #SYSTEM_SIZE=1363148800
 #FILE_CONTEXT=file_contexts_zenfone
