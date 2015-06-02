5ab22819fae5e9e3a93806a0d1a1b5acd32c58e4
Shaka Huang <shakalaca@gmail.com>
2015-06-02 13:38:22 +0800

    Update for T00F & T00G 5.0 upgrade
    Signed-off-by: Shaka Huang <shakalaca@gmail.com>

--- scripts/build.sh
+++ scripts/build.sh
@@ -17,7 +17,12 @@ wget -c $ROM_URL -O dl_rom.zip
 if [ -n "$OTA_URL" ]; then
   wget -c $OTA_URL -O ota.zip
 fi
-unzip dl_rom.zip -d unzipped_rom
+if [ -n "$ZIP_FILE" ]; then
+  unzip dl_rom.zip
+  unzip $ZIP_FILE -d unzipped_rom
+else
+  unzip dl_rom.zip -d unzipped_rom
+fi
 
 # just to get file_contexts
 #./unpack_intel UL-ASUS_T00F-WW-1.17.40.16-user/boot.img bzImage ramdisk.cpio.gz
@@ -27,6 +32,9 @@ unzip dl_rom.zip -d unzipped_rom
 
 mv unzipped_rom/system .
 ../scripts/create_link_file.sh unzipped_rom/META-INF/com/google/android/updater-script ../scripts/$LINK_PERM_SETUP_FILE
+if [ -f $ZIP_FILE ]; then
+  rm -rf $ZIP_FILE
+fi
 rm -rf unzipped_rom
 
 cp -R ../root/* system
--- scripts/setup.bash
+++ scripts/setup.bash
@@ -17,3 +17,15 @@ FILE_CONTEXT=file_contexts_z00a
 #SYSTEM_SIZE=2415919104
 #FILE_CONTEXT=file_contexts_z008
 #OTA_URL=http://fota.asus.com/delta_package/phone/WW_Z008/formal/WW_2.15.40.13_20150507_4648_user_rel-user-WW_Z008-WW_2.17.40.6_20150515_9759_user_rel-user-WW_Z008-20150518/WW_550_0507_0515_20150518.zip
+
+# ZenFone 5 T00F
+#LINK_PERM_SETUP_FILE=link_and_set_perm_t00f
+#ROM_URL=http://dlcdnet.asus.com/pub/ASUS/ZenFone/UL-ASUS_T00F-WW-3.23.40.52-user.zip
+#SYSTEM_SIZE=1363148800
+#FILE_CONTEXT=file_contexts_t00f
+
+# ZenFone 6 T00G
+#LINK_PERM_SETUP_FILE=link_and_set_perm_t00g
+#ROM_URL=http://dlcdnet.asus.com/pub/ASUS/ZenFone/UL-ASUS_T00G-WW-3.23.40.52-user.zip
+#SYSTEM_SIZE=1363148800
+#FILE_CONTEXT=file_contexts_t00g
