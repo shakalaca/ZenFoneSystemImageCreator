f5c2e80f17e49377db864accf85549dabd84eca7
Shaka Huang <shakalaca@gmail.com>
2015-06-03 08:40:50 +0800

    Remove ME572C
    Signed-off-by: Shaka Huang <shakalaca@gmail.com>

--- scripts/setup.bash
+++ scripts/setup.bash
@@ -1,9 +1,3 @@
-# ME572C
-#LINK_PERM_SETUP_FILE=link_and_set_perm_me572c
-#ROM_URL=http://dlcdnet.asus.com/pub/ASUS/EeePAD/ME572C/UL-K007-TW-12.14.1.9-user.zip
-#SYSTEM_SIZE=2042626048
-#FILE_CONTEXT=file_contexts_me572c
-
 # ZenFone 2 Z00A
 LINK_PERM_SETUP_FILE=link_and_set_perm_z00a
 ROM_URL=http://dlcdnet.asus.com/pub/ASUS/ZenFone/ZE551ML/UL-Z00A-WW-2.17.40.12-user.zip
