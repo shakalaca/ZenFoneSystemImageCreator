2ee4b52347e1c7dcae499a6f9d842b5d855e78c8
Shaka Huang <shakalaca@gmail.com>
2015-07-07 14:35:50 +0800

    Update Z008 to 2.19.40.23
    Signed-off-by: Shaka Huang <shakalaca@gmail.com>

--- scripts/setup.bash
+++ scripts/setup.bash
@@ -7,12 +7,12 @@ FILE_CONTEXT=file_contexts_z00a
 #OTA_URL=http://fota.asus.com/delta_package/phone/WW_Z00A/formal/WW_2.19.40.20_20150623_2193_user_rel-user-WW_Z00A-WW_2.19.40.22_20150627_5104_user_rel-user-WW_Z00A-20150627/WW_551_0623_0627_20150627.zip
 
 # ZenFone 2 Z008
-#VERSION=Z008_WW_2.19.40.13
+#VERSION=Z008_WW_2.19.40.23
 #LINK_PERM_SETUP_FILE=link_and_set_perm_z008
 #ROM_URL=http://dlcdnet.asus.com/pub/ASUS/ZenFone/ZE550ML/UL-Z008-WW-2.19.40.13-user.zip
 #SYSTEM_SIZE=2415919104
 #FILE_CONTEXT=file_contexts_z008
-#OTA_URL=http://fota.asus.com/delta_package/phone/WW_Z008/formal/WW_2.19.40.12_20150612_4176_user_rel-user-WW_Z008-WW_2.19.40.13_20150623_6113_user_rel-user-WW_Z008-20150625/WW_550_0612_0623_20150625.zip
+#OTA_URL=http://fota.asus.com/delta_package/phone/WW_Z008/formal/WW_2.19.40.13_20150623_6113_user_rel-user-WW_Z008-WW_2.19.40.23_20150701_1487_user_rel-user-WW_Z008-20150702/WW_550_0623_0701_20150702.zip
 
 # ZenFone 5 T00F
 #VERSION=T00F_WW_3.23.40.52
