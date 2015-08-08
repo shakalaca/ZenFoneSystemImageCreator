498e69ff132fc0f52053ff0010181a3552c9e984
Shaka Huang <shakalaca@gmail.com>
2015-08-08 13:23:33 +0800

    2.20.* update
    Signed-off-by: Shaka Huang <shakalaca@gmail.com>

--- scripts/setup.bash
+++ scripts/setup.bash
@@ -1,18 +1,18 @@
 # ZenFone 2 Z00A
-VERSION=Z00A_WW_2.19.40.22
+VERSION=Z00A_WW_2.20.40.58
 LINK_PERM_SETUP_FILE=link_and_set_perm_z00a
 ROM_URL=http://dlcdnet.asus.com/pub/ASUS/ZenFone/ZE551ML/UL-Z00A-WW-2.19.40.22-user.zip
 SYSTEM_SIZE=2415919104
 FILE_CONTEXT=file_contexts_z00a
-#OTA_URL=http://fota.asus.com/delta_package/phone/WW_Z00A/formal/WW_2.19.40.20_20150623_2193_user_rel-user-WW_Z00A-WW_2.19.40.22_20150627_5104_user_rel-user-WW_Z00A-20150627/WW_551_0623_0627_20150627.zip
+OTA_URL=http://fota.asus.com/delta_package/phone/WW_Z00A/formal/WW_2.19.40.22_20150627_5104_user_rel-user-WW_Z00A-WW_2.20.40.58_20150804_5878_user_rel-user-WW_Z00A-20150805/WW_551_0627_0804_20150805.zip
 
 # ZenFone 2 Z008
-#VERSION=Z008_WW_2.19.40.23
+#VERSION=Z008_WW_2.20.40.40
 #LINK_PERM_SETUP_FILE=link_and_set_perm_z008
-#ROM_URL=http://dlcdnet.asus.com/pub/ASUS/ZenFone/ZE550ML/UL-Z008-WW-2.19.40.13-user.zip
+#ROM_URL=http://dlcdnet.asus.com/pub/ASUS/ZenFone/ZE550ML/UL-Z008-WW-2.19.40.23-user.zip
 #SYSTEM_SIZE=2415919104
 #FILE_CONTEXT=file_contexts_z008
-#OTA_URL=http://fota.asus.com/delta_package/phone/WW_Z008/formal/WW_2.19.40.13_20150623_6113_user_rel-user-WW_Z008-WW_2.19.40.23_20150701_1487_user_rel-user-WW_Z008-20150702/WW_550_0623_0701_20150702.zip
+#OTA_URL=http://fota.asus.com/delta_package/phone/WW_Z008/formal/WW_2.19.40.23_20150701_1487_user_rel-user-WW_Z008-WW_2.20.40.40_20150804_5818_user_rel-user-WW_Z008-20150805/WW_550_0701_0804_20150805.zip
 
 # ZenFone 5 T00F
 #VERSION=T00F_WW_3.23.40.52
@@ -34,3 +34,5 @@ FILE_CONTEXT=file_contexts_z00a
 
 # Uncomment to add root survival function (fix su binary notification and preserve root after OTA upgrade)
 ROOT_SURVIVAL=1
+
+#SPLIT_SIZE=200m
