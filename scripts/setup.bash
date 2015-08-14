d725273f7615e1c323e5ab69a6226f3a657f068d
Shaka Huang <shakalaca@gmail.com>
2015-08-14 08:39:24 +0800

    Add full image of 2.20.40.59
    Signed-off-by: Shaka Huang <shakalaca@gmail.com>

--- scripts/setup.bash
+++ scripts/setup.bash
@@ -1,10 +1,10 @@
 # ZenFone 2 Z00A
 VERSION=Z00A_WW_2.20.40.59
 LINK_PERM_SETUP_FILE=link_and_set_perm_z00a
-ROM_URL=http://dlcdnet.asus.com/pub/ASUS/ZenFone/ZE551ML/UL-Z00A-WW-2.19.40.22-user.zip
+ROM_URL=http://dlcdnet.asus.com/pub/ASUS/ZenFone/ZE551ML/UL-Z00A-WW-2.20.40.59-user.zip
 SYSTEM_SIZE=2415919104
 FILE_CONTEXT=file_contexts_z00a
-OTA_URL=http://fota.asus.com/delta_package/phone/WW_Z00A/formal/WW_2.19.40.22_20150627_5104_user_rel-user-WW_Z00A-WW_2.20.40.59_20150807_7712_user_rel-user-WW_Z00A-20150810/WW_Z00A-20150627-20150807-20150810.zip
+#OTA_URL=http://fota.asus.com/delta_package/phone/WW_Z00A/formal/WW_2.19.40.22_20150627_5104_user_rel-user-WW_Z00A-WW_2.20.40.59_20150807_7712_user_rel-user-WW_Z00A-20150810/WW_Z00A-20150627-20150807-20150810.zip
 
 # ZenFone 2 Z008
 #VERSION=Z008_WW_2.20.40.41
