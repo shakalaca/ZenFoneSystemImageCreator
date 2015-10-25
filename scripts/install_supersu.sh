ec35632372aea3aaa8317cc435a327c3818c6812
Shaka Huang <shakalaca@gmail.com>
2015-10-25 23:59:12 +0800

    Update supersu
    Signed-off-by: Shaka Huang <shakalaca@gmail.com>

--- scripts/install_supersu.sh
+++ scripts/install_supersu.sh
@@ -13,16 +13,12 @@ cp system/xbin/su system/xbin/daemonsu
 rm system/bin/app_process
 ln -s /system/xbin/daemonsu system/bin/app_process
 
-if [ ! -z "$ROOT_SURVIVAL" ] || [ ! -z "$SLIM_DOWN" ]; then
-  if [ ! -f system/bin/app_process32_original ]; then
-    mv system/bin/app_process32 system/bin/app_process32_original
-  else 
-    rm system/bin/app_process32
-  fi
-  ln -s /system/xbin/daemonsu system/bin/app_process32
-  if [ ! -f system/bin/app_process_init ]; then
-    cp system/bin/app_process32_original system/bin/app_process_init
-  fi
-  
-  cp system/etc/install-recovery.sh system/etc/install-recovery.sh.bak  
+if [ ! -f system/bin/app_process64_original ]; then
+  mv system/bin/app_process64 system/bin/app_process64_original
+else 
+  rm system/bin/app_process64
+fi
+ln -s /system/xbin/daemonsu system/bin/app_process64
+if [ ! -f system/bin/app_process_init ]; then
+  cp system/bin/app_process64_original system/bin/app_process_init
 fi
