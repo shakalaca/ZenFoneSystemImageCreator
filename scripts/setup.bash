894e1bc9ae2ea471589897d505ab213af5e53e5c
Shaka Huang <shakalaca@gmail.com>
2015-10-25 23:59:40 +0800

    Add files for selfie
    Signed-off-by: Shaka Huang <shakalaca@gmail.com>

--- scripts/setup.bash
+++ scripts/setup.bash
@@ -1,38 +1,7 @@
-# ZenFone 2 Z00A
-VERSION=Z00A_WW_2.20.40.97
-LINK_PERM_SETUP_FILE=link_and_set_perm_z00a
+# ZenFone Selfie
+VERSION=Z00T_WW_1.12.40.382
 ROM_URL=http://dlcdnet.asus.com/pub/ASUS/ZenFone/ZE551ML/UL-Z00A-WW-2.20.40.97-user.zip
-SYSTEM_SIZE=2415919104
-FILE_CONTEXT=file_contexts_z00a
-#OTA_URL=http://fota.asus.com/delta_package/phone/WW_Z00A/formal/WW_2.20.40.90_20150903_4959_user_rel-user-WW_Z00A-WW_2.20.40.97_20150917_1572_user_rel-user-WW_Z00A-20150918/WW_Z00A-20150903-20150917-20150918.zip
-
-# ZenFone 2 Z008
-#VERSION=Z008_WW_2.20.40.92
-#LINK_PERM_SETUP_FILE=link_and_set_perm_z008
-#ROM_URL=http://dlcdnet.asus.com/pub/ASUS/ZenFone/ZE550ML/UL-Z008-WW-2.20.40.92-user.zip
-#SYSTEM_SIZE=2415919104
-#FILE_CONTEXT=file_contexts_z008
-#OTA_URL=http://fota.asus.com/delta_package/phone/WW_Z008/formal/WW_2.20.40.88_20150903_4922_user_rel-user-WW_Z008-WW_2.20.40.92_20150918_3181_user_rel-user-WW_Z008-20150918/WW_Z008-20150903-20150918-20150918.zip
-
-# ZenFone 5 T00F
-#VERSION=T00F_WW_3.23.40.52
-#LINK_PERM_SETUP_FILE=link_and_set_perm_t00f
-#ROM_URL=http://dlcdnet.asus.com/pub/ASUS/ZenFone/UL-ASUS_T00F-WW-3.23.40.52-user.zip
-##ROM_URL=http://dlcdnet.asus.com/pub/ASUS/ZenFone/A500CG/UL_ASUS_T00F_TW_3_23_40_52.zip
-##ZIP_FILE=UL-ASUS_T00F-TW-3.23.40.52-user.zip
-#SYSTEM_SIZE=1363148800
-#FILE_CONTEXT=file_contexts_t00f
-
-# ZenFone 6 T00G
-#VERSION=T00G_WW_3.23.40.52
-#LINK_PERM_SETUP_FILE=link_and_set_perm_t00g
-#ROM_URL=http://dlcdnet.asus.com/pub/ASUS/ZenFone/UL-ASUS_T00G-WW-3.23.40.52-user.zip
-##ROM_URL=http://dlcdnet.asus.com/pub/ASUS/ZenFone/A600CG/UL_ASUS_T00G_TW_3_23_40_52.zip
-##ZIP_FILE=UL-ASUS_T00G-TW-3.23.40.52-user.zip
-#SYSTEM_SIZE=1363148800
-#FILE_CONTEXT=file_contexts_t00g
-
-# Uncomment to add root survival function (fix su binary notification and preserve root after OTA upgrade)
-ROOT_SURVIVAL=1
+SYSTEM_SIZE=3221225472
+FILE_CONTEXT=file_contexts_z00t
 
 #SPLIT_SIZE=200m
