fa551a079c3349a05d5f12a5caa5737fa17f98e4
Shaka Huang <shakalaca@gmail.com>
2015-10-27 18:31:03 +0800

    Add ZE550KL
    Signed-off-by: Shaka Huang <shakalaca@gmail.com>

--- scripts/setup.bash
+++ scripts/setup.bash
@@ -4,4 +4,10 @@ ROM_URL=http://dlcdnet.asus.com/pub/ASUS/ZenFone/ZD551KL/UL-Z00T-WW-1.12.40.382-
 SYSTEM_SIZE=3221225472
 FILE_CONTEXT=file_contexts_z00t
 
+# ZenFone 2 Laser ZE550KL
+#VERSION=Z00L_WW_1.13.40.496
+#ROM_URL=http://dlcdnet.asus.com/pub/ASUS/ZenFone/ZE550KL/UL-Z00L-WW-1.13.40.496-user.zip
+#SYSTEM_SIZE=3221225472
+#FILE_CONTEXT=file_contexts_z00l
+
 #SPLIT_SIZE=200m
