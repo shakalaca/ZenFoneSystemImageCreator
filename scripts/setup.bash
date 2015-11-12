e1d03f7bec5e3ba2082bd398576c64061238e7e5
Shaka Huang <shakalaca@gmail.com>
2015-11-12 10:51:16 +0800

    Update 1.13.40.661 OTA
    Signed-off-by: Shaka Huang <shakalaca@gmail.com>

--- scripts/setup.bash
+++ scripts/setup.bash
@@ -1,7 +1,7 @@
 # ZenFone Selfie
-VERSION=Z00T_WW_1.13.40.531
+VERSION=Z00T_WW_1.13.40.661
 ROM_URL=http://dlcdnet.asus.com/pub/ASUS/ZenFone/ZD551KL/UL-Z00T-WW-1.12.40.382-user.zip
-OTA_URL=http://fota.asus.com/delta_package/phone/WW_Z00U/formal/WW_user_1.12.40.382_20150907-user-WW_Z00U-WW_user_1.13.40.531_20151006-user-WW_Z00U-20151006/WW_Z00T-1.12.40.382-1.13.40.531-20151006.zip
+OTA_URL=http://fota.asus.com/delta_package/phone/WW_Z00U/formal/WW_user_1.12.40.382_20150907-user-WW_Z00U-WW_user_1.13.40.661_20151103-user-WW_Z00U-20151105/WW_551_0907_1103_20151104_2.zip
 SYSTEM_SIZE=3221225472
 FILE_CONTEXT=file_contexts_z00t
 
