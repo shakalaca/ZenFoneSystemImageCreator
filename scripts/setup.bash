bd6882decbf4d5ce306ca52b1e4f8e0ea80ea594
Shaka Huang <shakalaca@gmail.com>
2015-11-28 22:25:02 +0800

    Update full ROM url
    Signed-off-by: Shaka Huang <shakalaca@gmail.com>

--- scripts/setup.bash
+++ scripts/setup.bash
@@ -1,7 +1,7 @@
 # ZenFone Selfie
 VERSION=Z00T_WW_1.14.40.695
-ROM_URL=http://dlcdnet.asus.com/pub/ASUS/ZenFone/ZD551KL/UL-Z00T-WW-1.13.40.661-user.zip
-OTA_URL=http://fota.asus.com/delta_package/phone/WW_Z00U/formal/WW_user_1.13.40.661_20151103-user-WW_Z00U-WW_user_1.14.40.695_20151114-user-WW_Z00U-20151116/WW_Z00U-1.13.40.661-1.14.40.695-20151116.zip
+ROM_URL=http://dlcdnet.asus.com/pub/ASUS/ZenFone/ZD551KL/UL-Z00T-WW-1.14.40.695-user.zip
+#OTA_URL=http://fota.asus.com/delta_package/phone/WW_Z00U/formal/WW_user_1.13.40.661_20151103-user-WW_Z00U-WW_user_1.14.40.695_20151114-user-WW_Z00U-20151116/WW_Z00U-1.13.40.661-1.14.40.695-20151116.zip
 SYSTEM_SIZE=3221225472
 FILE_CONTEXT=file_contexts_z00t
 
