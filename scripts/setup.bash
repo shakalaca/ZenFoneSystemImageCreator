ed07a12678609f6d2d0bf6b731a01b4df7f3db2c
Shaka Huang <shakalaca@gmail.com>
2016-01-28 18:47:22 +0800

    Z00A 2.20.40.165 & Z008 2.20.40.138
    Signed-off-by: Shaka Huang <shakalaca@gmail.com>

--- scripts/setup.bash
+++ scripts/setup.bash
@@ -1,16 +1,17 @@
 # ZenFone 2 Z00A
-VERSION=Z00A_WW_2.20.40.160
+VERSION=Z00A_WW_2.20.40.165
 LINK_PERM_SETUP_FILE=link_and_set_perm_z00a
-ROM_URL=http://dlcdnet.asus.com/pub/ASUS/ZenFone/ZE551ML/UL-Z00A-WW-2.20.40.139-user.zip
+ROM_URL=http://dlcdnet.asus.com/pub/ASUS/ZenFone/ZE551ML/UL-Z00A-WW-2.20.40.164-user.zip
 SYSTEM_SIZE=2415919104
 FILE_CONTEXT=file_contexts_z00a
 #OTA_URL=http://fota.asus.com/delta_package/phone/WW_Z00A/formal/WW_2.20.40.156_20151218_8265_user_rel-user-WW_Z00A-WW_2.20.40.160_20151225_1227_user_rel-user-WW_Z00A-20151225/WW_Z00A-20151218-20151225-20151225.zip
-OTA_URL=http://fota.asus.com/delta_package/phone/WW_Z00A/formal/WW_2.20.40.139_20151104_2652_user_rel-user-WW_Z00A-WW_2.20.40.160_20151225_1227_user_rel-user-WW_Z00A-20151225/WW_Z00A-20151104-20151225-20151225.zip
+#OTA_URL=http://fota.asus.com/delta_package/phone/WW_Z00A/formal/WW_2.20.40.139_20151104_2652_user_rel-user-WW_Z00A-WW_2.20.40.160_20151225_1227_user_rel-user-WW_Z00A-20151225/WW_Z00A-20151104-20151225-20151225.zip
+OTA_URL=http://fota.asus.com/delta_package/phone/WW_Z00A/formal/WW_2.20.40.164_20160107_6983_user_rel-user-WW_Z00A-WW_2.20.40.165_20160118_6541_user_rel-user-WW_Z00A-20160119_2/WW_Z00A-20160107-20160118-20160119_2.zip
 
 # ZenFone 2 Z008
-#VERSION=Z008_WW_2.20.40.135
+#VERSION=Z008_WW_2.20.40.138
 #LINK_PERM_SETUP_FILE=link_and_set_perm_z008
-#ROM_URL=http://dlcdnet.asus.com/pub/ASUS/ZenFone/ZE550ML/UL-Z008-WW-2.20.40.130-user.zip
+#ROM_URL=http://dlcdnet.asus.com/pub/ASUS/ZenFone/ZE550ML/UL-Z008-WW-2.20.40.138-user.zip
 #SYSTEM_SIZE=2415919104
 #FILE_CONTEXT=file_contexts_z008
 #OTA_URL=http://fota.asus.com/delta_package/phone/WW_Z008/formal/WW_2.20.40.130_20151104_2543_user_rel-user-WW_Z008-WW_2.20.40.135_20151218_0990_user_rel-user-WW_Z008-20151218/WW_Z008-20151104-20151218-20151218.zip
