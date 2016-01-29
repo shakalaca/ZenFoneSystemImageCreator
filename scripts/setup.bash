bdac739ed53b0c08892fa8ed3cafff2452357cba
Shaka Huang <shakalaca@gmail.com>
2016-01-29 17:08:35 +0800

    Update T00F & T00G 3.24.40.87
    Signed-off-by: Shaka Huang <shakalaca@gmail.com>

--- scripts/setup.bash
+++ scripts/setup.bash
@@ -14,16 +14,16 @@ FILE_CONTEXT=file_contexts_z00a
 #OTA_URL=http://fota.asus.com/delta_package/phone/WW_Z008/formal/WW_2.20.40.130_20151104_2543_user_rel-user-WW_Z008-WW_2.20.40.135_20151218_0990_user_rel-user-WW_Z008-20151218/WW_Z008-20151104-20151218-20151218.zip
 
 # ZenFone 5 T00F
-#VERSION=T00F_WW_3.24.40.78
+#VERSION=T00F_WW_3.24.40.87
 #LINK_PERM_SETUP_FILE=link_and_set_perm_t00f
-#ROM_URL=http://dlcdnet.asus.com/pub/ASUS/ZenFone/A500CG/UL-ASUS_T00F-WW-3.24.40.78-user.zip
+#ROM_URL=http://dlcdnet.asus.com/pub/ASUS/ZenFone/A500CG/UL-ASUS_T00F-WW-3.24.40.87-user.zip
 #SYSTEM_SIZE=1363148800
 #FILE_CONTEXT=file_contexts_t00f
 
 # ZenFone 6 T00G
-#VERSION=T00G_WW_3.24.40.78
+#VERSION=T00G_WW_3.24.40.87
 #LINK_PERM_SETUP_FILE=link_and_set_perm_t00g
-#ROM_URL=http://dlcdnet.asus.com/pub/ASUS/ZenFone/A600CG/UL-ASUS_T00G-WW-3.24.40.78-user.zip
+#ROM_URL=http://dlcdnet.asus.com/pub/ASUS/ZenFone/A600CG/UL-ASUS_T00F-WW-3.24.40.87-user.zip
 #SYSTEM_SIZE=1363148800
 #FILE_CONTEXT=file_contexts_t00g
 
