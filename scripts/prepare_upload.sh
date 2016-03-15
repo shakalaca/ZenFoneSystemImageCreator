0365eb832cc75fd622e7013f02aa52f33871426e
Shaka Huang <shakalaca@gmail.com>
2016-03-15 21:38:14 +0800

    Change md5 command to md5sum
    Signed-off-by: Shaka Huang <shakalaca@gmail.com>

--- scripts/prepare_upload.sh
+++ scripts/prepare_upload.sh
@@ -9,15 +9,15 @@ else
 fi
 
 cd $VERSION
-md5 -r boot.img > md5sum
-md5 -r droidboot.img >> md5sum
-md5 -r recovery.img >> md5sum
+md5sum boot.img > md5sum
+md5sum droidboot.img >> md5sum
+md5sum recovery.img >> md5sum
 
 if [ -n "$SPLIT_SIZE" ]; then
 #  7z a -mx9 -v$SPLIT_SIZE system.img.zip system.img
   zip ${VERSION}_system_w_root.zip system.img -s $SPLIT_SIZE
-  md5 -r *.z* >> md5sum
+  md5sum *.z* >> md5sum
 else
   zip system-${VARIANT}.img.zip system.img
-  md5 -r system-${VARIANT}.img.zip >> md5sum
+  md5sum system-${VARIANT}.img.zip >> md5sum
 fi
