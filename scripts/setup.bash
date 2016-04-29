d37bed5227e69b893d8e35f084aa7419d8a1138b
Shaka Huang <shakalaca@gmail.com>
2016-04-29 12:28:50 +0800

    Update ROM lists
    Signed-off-by: Shaka Huang <shakalaca@gmail.com>

--- scripts/setup.bash
+++ scripts/setup.bash
@@ -1,20 +1,37 @@
 # ZenFone Selfie
-VERSION=Z00T_WW_1.14.40.695
-ROM_URL=http://dlcdnet.asus.com/pub/ASUS/ZenFone/ZD551KL/UL-Z00T-WW-1.14.40.695-user.zip
-#OTA_URL=http://fota.asus.com/delta_package/phone/WW_Z00U/formal/WW_user_1.13.40.661_20151103-user-WW_Z00U-WW_user_1.14.40.695_20151114-user-WW_Z00U-20151116/WW_Z00U-1.13.40.661-1.14.40.695-20151116.zip
+VERSION=Z00T_WW_1.15.40.1238
+ROM_URL=http://dlcdnet.asus.com/pub/ASUS/ZenFone/ZD551KL/UL-Z00T-WW-1.15.40.1238-user.zip
 SYSTEM_SIZE=3221225472
 FILE_CONTEXT=file_contexts_z00t
 
-# ZenFone 2 Laser ZE550KL
-#VERSION=Z00L_WW_1.16.40.763
-#ROM_URL=http://dlcdnet.asus.com/pub/ASUS/ZenFone/ZE550KL/UL-Z00L-WW-1.16.40.763-user.zip
-#SYSTEM_SIZE=3221225472
-#FILE_CONTEXT=file_contexts_z00l
-
-# ZenFone 2 Laser ZE601KL
-#VERSION=Z00T_WW_1.15.40.825
-#ROM_URL=http://dlcdnet.asus.com/pub/ASUS/ZenFone/ZE601KL/UL-Z00T-WW-1.15.40.825-user.zip
+# ZenFone 2 Laser ZE551KL
+#VERSION=Z00T_WW_1.17.40.1106
+#ROM_URL=http://dlcdnet.asus.com/pub/ASUS/ZenFone/ZE551KL/UL-Z00T-WW-1.17.40.1106-user.zip
 #SYSTEM_SIZE=
 #FILE_CONTEXT=file_contexts_z00t
+
+# ZenFone 2 Laser ZE601KL
+#VERSION=Z00T_WW_1.16.40.1020
+#ROM_URL=http://dlcdnet.asus.com/pub/ASUS/ZenFone/ZE601KL/UL-Z00T-WW-1.16.40.1020-user.zip
+#SYSTEM_SIZE=
+#FILE_CONTEXT=file_contexts_z00t
+
+# ZenFone 2 Laser ZE550KL
+#VERSION=Z00L_WW_1.17.40.1234
+#ROM_URL=http://dlcdnet.asus.com/pub/ASUS/ZenFone/ZE550KL/UL-Z00L-WW-1.17.40.1234-user.zip
+#SYSTEM_SIZE=
+#FILE_CONTEXT=file_contexts_z00l
+
+# ZenFone 2 Laser ZE500KL
+#VERSION=Z00E_WW_12.8.5.222
+#ROM_URL=http://dlcdnet.asus.com/pub/ASUS/ZenFone/ZE500KL/UL-ASUS_Z00E-WW-12.8.5.222-user.zip
+#SYSTEM_SIZE=
+#FILE_CONTEXT=file_contexts_z00e
+
+# ZenFone Max ZC550KL
+#VERSION=Z010_WW_12.8.10.56
+#ROM_URL=http://dlcdnet.asus.com/pub/ASUS/ZenFone/ZC550KL/UL-ASUS_Z010-WW-12.8.10.56-user.zip
+#SYSTEM_SIZE=
+#FILE_CONTEXT=file_contexts_z010
 
 #SPLIT_SIZE=200m
